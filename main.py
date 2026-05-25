78a61596b7dadb1873da163cc5a8cbe6dfee5769
babyzed <zaheerhasanpoor@gmail.com>
2026-05-25 18:04:41 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Enter the necessary library
+
+import random
+import string
+
+# Define the characters from which the password is made
+
+chars = string.ascii_letters + string.digits + "!@#$%^&*()-=_+"
+
+# Getting the length of the password from the user
+
+length = int(input("password length: "))
+
+# Random password build
+
+password = "".join(random.choice(chars) for _ in range(length))
+
+# View and result
+print("Generated password: ")
+print(password)
